83f97879800d59f613f301670e5e1a15f4365e5a
Fajar Geran Arifin <geranuser@gmail.com>
2026-03-09 07:52:34 +0700

    update cofig js

--- next.config.js
+++ next.config.js
@@ -6,6 +6,14 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:3333/api/:path*",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
